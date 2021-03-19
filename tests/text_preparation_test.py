44f506e9294e18dfeb1498d5e10491855ceac00b
Konstantin <k.gazha@mininform74.ru>
2021-03-19 15:25:12 +0500

    Add a new test, refactor old tests

--- tests/text_preparation_test.py
+++ tests/text_preparation_test.py
@@ -7,12 +7,18 @@ class TestTextPreparation(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(TestTextPreparation, self).__init__(*args, **kwargs)
         self.text = "Привет из Южного Урала!"
+        self.cleaned_text = "Привет из Южного Урала"
 
     def test_text_preparation(self):
-        cleaned_text = "Привет из Южного Урала"
-        normalized_words = get_normalized_words(cleaned_text)
-        assert normalized_words == ["привет", "из", "южный", "урал"]
+        normalized_words = get_normalized_words(self.cleaned_text)
+        expected_result = ["привет", "из", "южный", "урал"]
+        self.assertEqual(normalized_words, expected_result)
+
+        normalized_words = get_normalized_words(self.cleaned_text, excluded_pos=["ADJF"])
+        expected_result = ["привет", "из", "урал"]
+        self.assertEqual(normalized_words, expected_result)
 
     def test_get_cleaned_text(self):
-        cleaned_text = get_cleaned_text(self.text)
-        assert cleaned_text == "Привет из Южного Урала"
+        actual_text = get_cleaned_text(self.text)
+        self.assertEqual(actual_text, self.cleaned_text)
+
