0922649ceb698b7256fd3519616c36d80fdbdd1c
Konstantin <k.gazha@mininform74.ru>
2021-03-22 17:26:14 +0500

    Refactor text preparation methods

---
+++ src/qa_reader.py
@@ -0,0 +1,7 @@
+from src.excel_parser import ExcelParser
+
+
+full_file_name = r"C:\Users\gazhakv\Documents\Вопросы  Робот  v17.xlsx"
+excelParser = ExcelParser()
+excelParser.load_data_source(full_file_name)
+data = excelParser.extract_data()
--- src/text_preparation.py
+++ src/text_preparation.py
@@ -7,23 +7,26 @@ morph = pymorphy2.MorphAnalyzer()
 russian_stopwords = stopwords.words("russian")
 
 
-def get_normalized_words(text: str, excluded_pos=None) -> []:
-    if excluded_pos is None:
-        excluded_pos = []
-    normalized_words = []
-    for word in text.split():
-        normal_form = None
-        for parsed_word in morph.parse(word):
-            if parsed_word.tag.POS in excluded_pos:
-                continue
-            normal_form = parsed_word.normal_form
-            break
-        if normal_form:
-            normalized_words.append(normal_form)
-    return normalized_words
+class TextHandler:
+    def __init__(self, text: str):
+        self.text = text
+        self.cleaned_text = ""
+        self.normalized_words = []
 
+    def get_normalized_words(self, excluded_pos=None):
+        if excluded_pos is None:
+            excluded_pos = []
+        self.normalized_words = []
+        for word in self.text.split():
+            normal_form = None
+            for parsed_word in morph.parse(word):
+                if parsed_word.tag.POS in excluded_pos:
+                    continue
+                normal_form = parsed_word.normal_form
+                break
+            if normal_form:
+                self.normalized_words.append(normal_form)
 
-def get_cleaned_text(text: str) -> str:
-    translator = str.maketrans({key: " " for key in punctuation})
-    cleaned_text = " ".join(text.translate(translator).split())
-    return cleaned_text
+    def get_cleaned_text(self, text: str):
+        translator = str.maketrans({key: " " for key in punctuation})
+        self.cleaned_text = " ".join(text.translate(translator).split())
