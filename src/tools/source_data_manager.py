a1cf01ad56c511973a953c84619f0c9d4424a546
Konstantin <kgazha@gmail.com>
2021-03-30 11:52:27 +0500

    Change logic of saving data in pickle

--- src/tools/source_data_manager.py
+++ src/tools/source_data_manager.py
@@ -1,26 +1,10 @@
 import pickle
+from typing import Union
+
 from pandas import DataFrame
-from functools import singledispatch
-
-from src.interfaces.data_handler_interface import DataHandlerInterface
 
 
-@singledispatch
-def save_to_pickle(data_handler: DataHandlerInterface, source_filename: str, destination_filename: str):
-    data_handler.load_data_source(source_filename)
-    df = data_handler.extract_data()
-    with open(destination_filename, "wb+") as file:
-        pickle.dump(df, file)
-
-
-@save_to_pickle.register(DataFrame)
-def _save_to_pickle(df: DataFrame, destination_filename: str) -> None:
-    with open(destination_filename, "wb+") as file:
-        pickle.dump(df, file)
-
-
-@save_to_pickle.register(list)
-def _save_to_pickle(data: list, destination_filename: str) -> None:
+def save_to_pickle(data: Union[DataFrame, list], destination_filename: str) -> None:
     with open(destination_filename, "wb+") as file:
         pickle.dump(data, file)
 
