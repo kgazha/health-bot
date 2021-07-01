3454794edc206bf7bbf6200e3b66ff9a944c8807
Konstantin <kgazha@gmail.com>
2021-07-01 13:44:25 +0500

    Fix test for DataFrameHandler

--- tests/df_handler_test.py
+++ tests/df_handler_test.py
@@ -1,4 +1,4 @@
-from src.analysis.df_manager import DataFrameManager
+from src.handlers.df_handler import DataFrameHandler
 from src.handlers.base_text_handler import TextHandler
 
 from pandas import DataFrame
@@ -6,25 +6,25 @@ from pandas.testing import assert_frame_equal
 import unittest
 
 
-class DataFrameManagerTest(unittest.TestCase):
+class DataFrameHandlerTest(unittest.TestCase):
 
     def __init__(self, *args, **kwargs):
-        super(DataFrameManagerTest, self).__init__(*args, **kwargs)
+        super(DataFrameHandlerTest, self).__init__(*args, **kwargs)
         self.df = DataFrame([["0", "1|3", "2"], ["1", "2", "3"], ["6", "7|1", "8"]])
-        self._df_manager = DataFrameManager(self.df, TextHandler())
+        self._df_handler = DataFrameHandler(TextHandler())
 
     def test_transform_by_splitting_column(self):
-        self._df_manager.transform_by_splitting_column(1, "|")
+        result = self._df_handler.transform_by_splitting_column(self.df, 1, "|")
         expected_df = DataFrame([["0", "1", "2"],
                                  ["0", "3", "2"],
                                  ["1", "2", "3"],
                                  ["6", "7", "8"],
                                  ["6", "1", "8"]])
-        assert_frame_equal(self._df_manager.df, expected_df)
+        assert_frame_equal(result, expected_df)
 
     def test_cut_column_by_splitting(self):
-        self._df_manager.cut_column_by_splitting(1, "|", 1)
+        self._df_handler.cut_column_by_splitting(self.df, 1, "|", 1)
         expected_df = DataFrame([["0", "1", "2"],
                                  ["1", "2", "3"],
                                  ["6", "7", "8"]])
-        assert_frame_equal(self._df_manager.df, expected_df)
+        assert_frame_equal(self.df, expected_df)
